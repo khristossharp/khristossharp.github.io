9b29ccd6433bf2eb0a1ec1c073c341f87a29960f
Khris <hrek1994@gmail.com>
2025-03-23 19:08:59 -0400

    Initial Portfolio Deploy

---
+++ README.md
@@ -0,0 +1,50 @@
+# Portfolio
+
+This is a Blazor-based static portfolio website that includes personal information and a tennis game project. The portfolio highlights my skills, experience, and projects, offering a comprehensive overview of my work and capabilities.
+
+## Key Features
+
+- **About Me**: An introduction to who I am.
+- **Education**: Information about my academic background.
+- **IT Experience**: Details of my professional experience in the IT industry.
+- **Skills**: A list of the technical skills I possess.
+- **Certifications**: Relevant certifications that highlight my qualifications.
+- **Hobbies**: A section that shares my personal interests.
+- **Tennis Game Project**: A fun interactive game where users can play against the computer.
+
+## Tennis Game
+
+The tennis game is built using C#, ASP.NET Core, HTML/CSS, and MudBlazor components. The game consists of the user playing against the computer. Here's how it works:
+
+- **Serve**: The user selects a side to serve. If the computer guesses correctly, it gets a point. If not, the user wins the point.
+- **Gameplay**: After the serve, the ball moves to the computer's side, and the player must guess where the ball will go to continue the game.
+
+## Live Demo
+
+You can view my portfolio and play the game by visiting the live demo at:  
+[Portfolio Live Demo](https://khristossharp.github.io/)
+
+*Note: This project is hosted and cannot be run locally as it was published directly from the Blazor app.*
+
+## Source Code Reference
+
+For the full Blazor project, please visit the [Blazor Project Repository](https://github.com/khristossharp/Blazor-Portfolio)
+
+## Technologies Used
+
+- **Blazor**: For building interactive web applications.
+- **ASP.NET Core**: The backend framework powering the app.
+- **MudBlazor**: For providing a great set of UI components to enhance the frontend experience.
+MudBlazor is licensed under the MIT License. For more information, you can review the license [LICENSE](https://github.com/MudBlazor/MudBlazor/blob/dev/LICENSE/)
+
+
+## Acknowledgments
+
+- **MudBlazor**: For providing a great set of UI components to enhance the frontend experience.
+- **Blazor**: For simplifying web development with C# and .NET.
+- **ASP.NET Core**: The backend framework that enables fast and scalable web applications.
+
+---
+
+Thank you for visiting my portfolio!
+
---
+++ js/site.js
@@ -0,0 +1,122 @@
+﻿
+function moveBallWhenServe(position)
+{
+    var ball = document.getElementById('ball');
+    var racket = document.getElementById('racket');
+
+    if (!position)
+    {
+        ball.style.top = '';
+        ball.style.left = '40%';
+        ball.style.bottom = '2%';
+
+        //Added racket to move center after it hits the ball
+        racket.style.top = '2%';
+        racket.style.left = '40%';
+
+    }
+    else {
+
+    switch (position)
+    {
+        case 'Left':
+            ball.style.bottom = '';
+            ball.style.left = '10%';
+            ball.style.top = '2%';
+            break;
+        case 'Center':
+            ball.style.bottom = '';
+            ball.style.left = '40%';
+            ball.style.top = '2%';
+            break;
+        case 'Right':
+            ball.style.bottom = '';
+            ball.style.left = '70%'; 
+            ball.style.top = '2%';
+            break;
+        }
+    }
+}
+
+function moveBallWhenHit(position)
+{
+  
+    switch (position) {
+        case 'Left':
+            ball.style.top = '';
+            ball.style.left = '10%';
+            ball.style.bottom = '2%';
+            break;
+        case 'Center':
+            ball.style.top = '';
+            ball.style.left = '40%';
+            ball.style.bottom = '2%';
+            break;
+        case 'Right':
+            ball.style.top = '';
+            ball.style.left = '70%';
+            ball.style.bottom = '2%';
+            break;
+    }
+}
+
+function setTheBallForComputer()
+{
+    ball.style.top = '';
+    ball.style.bottom = '85%';
+    ball.style.left = '40%';
+
+    racket.style.bottom = '';
+    racket.style.left = '40%';
+    racket.style.top = '80%';
+
+}
+
+
+function moveRacketToHit(position, isItPlayerRacketHit)
+{
+
+    if (isItPlayerRacketHit)
+    {
+        switch (position) {
+            case 'Left':
+                
+                racket.style.left = '10%';
+                racket.style.top = '75%';
+                break;
+            case 'Center':
+                
+                racket.style.left = '40%';
+                racket.style.top = '70%';
+                break;
+            case 'Right':
+                
+                racket.style.left = '70%';
+                racket.style.top = '75%';
+                break;
+        }
+    } else
+    {
+        switch (position)
+        {
+            case 'Left':
+                racket.style.bottom = '';
+                racket.style.top = '2%';
+                racket.style.left = '10%';
+
+                break;
+            case 'Center':
+                racket.style.bottom = '';
+                racket.style.top = '9%';
+                racket.style.left = '40%';
+
+                break;
+            case 'Right':
+                racket.style.bottom = '';
+                racket.style.top = '2%';
+                racket.style.left = '70%';
+
+                break;
+        }
+    }
+}
